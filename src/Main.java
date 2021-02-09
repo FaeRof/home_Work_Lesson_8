4e3f3b30f9900f1fbe86d3abad8eafb18bf112b0
FaeRof <chernysheya@mail.ru>
2021-02-09 21:46:58 +0300

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
 
 public class Main {
 
+
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String st1 = scanner.nextLine();
